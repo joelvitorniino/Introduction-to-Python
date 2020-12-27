d3e336ca14e95627565933f5c75ccaad158c7d11
Niino Matthew <76634277+NiinoTM@users.noreply.github.com>
2020-12-27 05:07:53 -0300

    Add files via upload

---
+++ training.py
@@ -0,0 +1,15 @@
+"""
+Well, I'm trying to learn python language, to use it to help in making useful applications.
+"""
+x=1 #setting a value to the variable
+if 5 + x > 5: #conditional
+ print("First code") #Use indentation so that this function is inside the function above (if)
+ 
+greetings = ["Good Morning!", "Good Afternoon!", "Good Evening!"]
+x, y, z = greetings #note that x had already a value (5), but this overwrote the value.
+
+print("These are the greetings that may fit your actual time:")
+for q in x + y + z:
+ print(q)
+
+print("GooD" in x)
